b3edfe7b69a2cb9487d2c717ce098bf6ebb67c49
Scott Currell <scottcurrell@gmail.com>
2018-04-29 19:30:31 -0600

    New display function and add comments.

--- cs_165/cs165_assignments/myAssign01.cpp
+++ cs_165/cs165_assignments/myAssign01.cpp
@@ -12,7 +12,10 @@
 *
 *    Estimated:  3.0 hrs   
 *    Actual:     0.0 hrs
-*      Please describe briefly what was the most difficult part.
+* Please describe briefly what was the most difficult part:
+*    I struggled with matching the letter of each DNA sequence to the
+*    comparison sequence. My nested for loops had transposed boolean
+*    variables. It took me quite a lot of time to find the error.
 ************************************************************************/
 
 #include <iostream>
@@ -31,11 +34,15 @@ void matchDNASequences(char relatives[][256],
                        char dnaSequences[][11],
                        int numRelatives,
                        string dnaSequence);
+void displayResult(string relativeName, int percentMatch);
 
-/**********************************************************************
- * Add text here to describe what the function "main" does. Also don't forget
- * to fill this out with meaningful text or YOU WILL LOSE POINTS.
- ***********************************************************************/
+/***********************************************************************
+ * Prompt the user for a 10-character DNA marker sequence, a number of
+ * potential relatives, the names of each of these potential relatives, and
+ * a 10-character DNA marker sequence of each of the potential relatives.
+ * Display each potential relative along with the percentage that their
+ * sequence matched the target user.
+ ************************************************************************/
 int main()
 {
    // base DNA sequence
@@ -47,18 +54,28 @@ int main()
    // relatives' DNA sequences [max num possible relatives][DNA sequences]
    char relativesDNASequences[50][11];
 
-   getRelativesNames(relatives, numRelatives);
-   getRelativesDNASequences(relatives, relativesDNASequences, numRelatives);
-   matchDNASequences(relatives, relativesDNASequences, numRelatives, dnaSequence);
+   // prompt for relatives' names
+   getRelativesNames(relatives,
+                     numRelatives);
+   // prompt for relatives' DNA sequences
+   getRelativesDNASequences(relatives,
+                            relativesDNASequences,
+                            numRelatives);
+   // match DNA sequences
+   matchDNASequences(relatives,
+                     relativesDNASequences,
+                     numRelatives,
+                     dnaSequence);
 
+   // end function
    return 0;
 }
 
-/**
+/***********************************************************************
  * GET DNA SEQUENCE
  * Prompt user for a DNA sequence as a base for comparison.
- * @return dnaSequence [String]
- */
+ *    OUTPUT: dnaSequence [String]
+ ************************************************************************/
 string getDNASequence()
 {
    // DNA sequence
@@ -73,11 +90,11 @@ string getDNASequence()
    return dnaSequence;
 }
 
-/**
+/***********************************************************************
  * GET NUMBER OF RELATIVES
  * Prompt user for a number of relatives.
- * @return numRelatives[Int]
- */
+ *    OUTPUT: numRelatives[Int]
+ ************************************************************************/
 int getNumRelatives()
 {
    // number of relatives
@@ -92,20 +109,21 @@ int getNumRelatives()
    return numRelatives;
 }
 
-/**
+/***********************************************************************
  * GET RELATIVES' NAMES
  * Prompt user for relatives' names
- * @return relatives[Array]
- */
+ *    INPUT: relatives[Array],
+ *           numRelatives[Int]
+ ************************************************************************/
 void getRelativesNames(char relatives[][256],
                        int numRelatives)
 {
    // start with a new line return
    cout << endl;
 
-   // prompt user for 'n' number of names (based on what s/he provided) as a
+   // prompt user for a number of names (based on what s/he provided) as a
    // number of relatives
-   for(int i = 0; i < numRelatives; i++)
+   for (int i = 0; i < numRelatives; i++)
    {
       // prompt user for relatives' names
       cout << "Please enter the name of relative #" << i + 1 << ": ";
@@ -117,11 +135,13 @@ void getRelativesNames(char relatives[][256],
    return;
 }
 
-/**
+/***********************************************************************
  * GET RELATIVES' DNA SEQUENCES
  * Prompt user for relatives' DNA sequences
- * @return dnaSequences[Array]
- */
+ *    INPUT: relatives[Array],
+ *           dnaSequences[Array],
+ *           numRelatives[Int]
+ ************************************************************************/
 void getRelativesDNASequences(char relatives[][256],
                               char dnaSequences[][11],
                               int numRelatives)
@@ -129,9 +149,9 @@ void getRelativesDNASequences(char relatives[][256],
    // start with a new line return
    cout << endl;
 
-   // prompt user for 'n' number of DNA sequences (based on what s/he
+   // prompt user for a number of DNA sequences (based on what s/he
    // provided) as a number of relatives
-   for(int i = 0; i < numRelatives; i++)
+   for (int i = 0; i < numRelatives; i++)
    {
       // prompt user for relatives' DNA sequences
       cout << "Please enter the DNA sequence for " << relatives[i] << ": ";
@@ -143,6 +163,15 @@ void getRelativesDNASequences(char relatives[][256],
    return;
 }
 
+/***********************************************************************
+ * MATCH DNA SEQUENCES
+ * Compare each relatives' DNA sequence against the one provided by the user
+ * and pass a percent match to the display function.
+ *    INPUT: relatives[Array],
+ *           dnaSequences[Array],
+ *           numRelatives[Int],
+ *           dnaSequence[String]
+ ************************************************************************/
 void matchDNASequences(char relatives[][256],
                        char dnaSequences[][11],
                        int numRelatives,
@@ -151,21 +180,48 @@ void matchDNASequences(char relatives[][256],
    // start with a new line return
    cout << endl;
 
+   // starting percentage
    int percentMatch = 0;
+   // name of relative after each iteration
+   string relativeName;
 
-   for(int i = 0; i < numRelatives; i++)
+   // iterate over each relative
+   for (int i = 0; i < numRelatives; i++)
    {
-      for(int j = 0; j < 10; j++)
+      // iterate over each letter in each relatives' DNA sequence
+      for (int j = 0; j < 10; j++)
       {
-         if(dnaSequences[i][j] == dnaSequence[j])
+         // if letter from sequence is a match
+         if (dnaSequences[i][j] == dnaSequence[j])
          {
+            // increment the percentage
             percentMatch++;
          }
       }
-      cout << "Percent match for " << relatives[i] << ": " << percentMatch * 10 << "%\n";
+
+      // update relatives' names
+      relativeName = relatives[i];
+      // print the resultant relatives' match percentage to the screen
+      displayResult(relativeName, percentMatch);
+      // reset the count
       percentMatch = 0;
    }
 
    // end function
    return;
+}
+
+/***********************************************************************
+ * DISPLAY RESULT
+ * Print each relative's name and match percentage to the screen
+ *    INPUT: relativeName[String],
+ *           percentMatch[Int]
+ ************************************************************************/
+void displayResult(string relativeName, int percentMatch)
+{
+   cout << "Percent match for "
+        << relativeName
+        << ": "
+        << percentMatch * 10
+        << "%\n";
 }
