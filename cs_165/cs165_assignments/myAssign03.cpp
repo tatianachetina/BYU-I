26bbaec3710000e4ac21c133ae2b0489059816c3
Scott Currell <scottcurrell@gmail.com>
2018-05-11 13:49:21 -0600

    completed week03 assignment

--- cs_165/cs165_assignments/myAssign03.cpp
+++ cs_165/cs165_assignments/myAssign03.cpp
@@ -8,17 +8,19 @@
 *    Prompt the user for the name of a file containing user access records.
 *    Prompt the user for a start and end time for comparison. Display a list
 *    of all files that were accessed during that period along with the users
-*    who accessed them.
+*    who accessed them. Validate data: check start/end times, missing
+*    parameters, and corrupt data.
 *
 *    Estimated:  6.0 hrs   
-*    Actual:     12.0 hrs
+*    Actual:     4.0 hrs
 * Please describe briefly what was the most difficult part:
-*    I struggled more with last week's assignment. The only thing that
-*    tripped me up this week was figuring out how to iterate through the
-*    array of records. I didn't know how to find the number of lines in
-*    the file. I ended up creating a counter in the read function to keep
-*    track of how many lines were read. Maybe there's a better way, but this
-*    worked for me
+*    I thought I'd be able to plop in the logic to validate the data. That
+*    ended up being more difficult than I thought; mostly because it was hard
+*    for me to keep track of all the moving pieces while refactoring last
+*    week's assignment. I ended up starting from scratch and copy/pasting my
+*    code from last week, one function at a time, so I could keep it all
+*    straight. Other that that, it went pretty well. I also included
+*    suggestions from last week's feedback.
 ************************************************************************/
 
 #include <cassert>  // assert lib
@@ -45,22 +47,26 @@ struct AccessRecord
 struct UserData
 {
    string sourceFile;
+   int numRecords;
    long startTime;
    long endTime;
 };
 
 // prototypes
-void promptForFilename(UserData & file);
-void parseFile(UserData & userData);
+void promptForFilename(UserData &file);
+bool parseFile(AccessRecord logRecords[],
+               UserData &userData);
 void parseLine(string line,
                AccessRecord logRecords[],
                int recordNumber) throw(string);
-void promptForStartAndEndTimes(UserData & times);
+void promptForStartAndEndTimes(UserData &times);
+void displayAccessRecords(AccessRecord logRecords[],
+                          UserData &userData);
 
 /***********************************************************************
  * MAIN
  * This is the entry point and driver for the program.
- *    Vars: input [struct] - user-defined data and number of records
+ *    Vars: input      [struct] - user-defined data and number of records
  *          logRecords [array] - access records: filename
  *                                               username
  *                                               timestamp
@@ -69,20 +75,37 @@ int main()
 {
    // new instance of UserData
    UserData input;
+   // new instance of AccessRecord: log records
+   AccessRecord logRecords[MAX_NUM_RECORDS];
 
-   // get the source file name
-   promptForFilename(input);
-   // read access records from source file
-   parseFile(input);
+   // valid source file: true/false
+   bool isGoodFile;
+
+   // re-prompt if invalid file
+   do
+   {
+      // get the source file name
+      promptForFilename(input);
+      // check if source file is good
+      isGoodFile = parseFile(logRecords, input);
+   }
+   while (!isGoodFile);
+
    // get start/end times
    promptForStartAndEndTimes(input);
    // display matched records on screen
-   // displayAccessRecords(logRecords, input);
+   displayAccessRecords(logRecords, input);
 
    return 0;
 }
 
-void promptForFilename(UserData & file)
+/***********************************************************************
+ * PROMPT FOR FILENAME
+ * Get the source filename from the user. The file that contains the user
+ * logs.
+ *    OUTPUT: sourceFile [string]
+ ************************************************************************/
+void promptForFilename(UserData &file)
 {
    cout << "Enter the access record file: ";
    cin >> file.sourceFile;
@@ -90,7 +113,15 @@ void promptForFilename(UserData & file)
    return;
 }
 
-void parseFile(UserData & userData)
+/***********************************************************************
+ * PARSE FILE
+ * Parse the file, extracting data one line at a time. Pass extracted lines
+ * to parseLine() for validation.
+ *    INPUT: numRecords  [int]
+ *    OUTPUT: logRecords [array]
+ ************************************************************************/
+bool parseFile(AccessRecord logRecords[],
+               UserData &userData)
 {
    // number of lines in file
    int lineNumber = 0;
@@ -104,23 +135,23 @@ void parseFile(UserData & userData)
       // tell user there's something wrong with the file
       cout << "invalid file: " << userData.sourceFile << endl;
 
-      return;
+      return false;
    }
 
+   // line retrieved from file
    string line;
 
    // read content to the end of the file one line at a time
    while (getline(fin, line))
    {
-      // fin.ignore(256, '\n');
-      AccessRecord logRecords[MAX_NUM_RECORDS];
-
       try
       {
+         // check for corrupt data and update logs array with valid data
          parseLine(line, logRecords, lineNumber);
       }
       catch (string text)
       {
+         // tell user there's something wrong with the data
          cout << "Error parsing line: " << text << endl;
       }
 
@@ -128,37 +159,120 @@ void parseFile(UserData & userData)
       lineNumber++;
    }
 
+   // update structure with number of user records
+   // minus 1 since array is zero-indexed
+   userData.numRecords = lineNumber - 1;
+
    // close the read file
    fin.close();
 
-   return;
+   return true;
 }
 
+/***********************************************************************
+ * PARSE LINE
+ * Parse each line to check for errors and/or corrupt data. Update the logs
+ * array with valid data.
+ *    INPUT: line         [string]
+ *    INPUT: logRecords   [array]
+ *    INPUT: recordNumber [int]
+ ************************************************************************/
 void parseLine(string line,
                AccessRecord logRecords[],
                int recordNumber) throw(string)
 {
 
+   // read data one line at a time
    stringstream buffer(line);
 
+   // update logs array
    buffer >> logRecords[recordNumber].filename
           >> logRecords[recordNumber].username
           >> logRecords[recordNumber].timestamp;
 
+   // validate timestamps
    if (logRecords[recordNumber].timestamp < 1000000000 ||
        logRecords[recordNumber].timestamp > 10000000000)
    {
+      // failed or corrupted data
       throw line;
    }
-
 }
 
-void promptForStartAndEndTimes(UserData & times)
+/***********************************************************************
+ * PROMPT FOR START AND END TIMES
+ * Get the start and end times from the user. Used to get a range of logs.
+ *    OUTPUT: input.startTime [long]
+ *            input.endTime   [long]
+ ************************************************************************/
+void promptForStartAndEndTimes(UserData &times)
 {
    cout << "\nEnter the start time: ";
    cin >> times.startTime;
+
+   while (cin.fail())
+   {
+      cin.clear();
+      // skip over up to 256 characters
+      // or until a newline char '\n' is encountered
+      cin.ignore(256, '\n');
+      cout << "Invalid input.\nEnter the start time: ";
+      cin >> times.startTime;
+   }
+
    cout << "Enter the end time: ";
    cin >> times.endTime;
+
+   // I'm sure there's a better way to do this while loop (since it's
+   // duplicated from lines 213 - 221) but I couldn't figure out how to make
+   // it a modular. Could you provide insight when you grade this, please?
+   while (cin.fail())
+   {
+      cin.clear();
+      // skip over up to 256 characters
+      // or until a newline char '\n' is encountered
+      cin.ignore(256, '\n');
+      cout << "Invalid input.\nEnter the end time: ";
+      cin >> times.endTime;
+   }
+
+   return;
+}
+
+/***********************************************************************
+ * DISPLAY ACCESS RECORDS
+ * Display a table of access records within a given time range.
+ *    INPUT: input [struct]
+ *           logRecords [array]
+ ************************************************************************/
+void displayAccessRecords(AccessRecord logRecords[], UserData &userData)
+{
+   cout << endl
+        << "The following records match your criteria:"
+        << endl << endl;
+
+   // header
+   cout << setw(15) << "Timestamp"
+        << setw(20) << "File"
+        << setw(20) << "User" 
+        << endl;
+   // separator
+   cout << "--------------- ------------------- -------------------" << endl;
+
+   // write each record to the screen
+   for (int i = 0; i < userData.numRecords; i++)
+   {
+      // only if they're within the specified time range
+      if (logRecords[i].timestamp >= userData.startTime &&
+          logRecords[i].timestamp <= userData.endTime)
+      {
+         cout << setw(15) << logRecords[i].timestamp
+              << setw(20) << logRecords[i].filename
+              << setw(20) << logRecords[i].username << endl;
+      }
+   }
+
+   cout << "End of records\n";
 
    return;
 }
