df84af45efe34055bd16a06971808e501075e3bc
Scott Currell <scottcurrell@gmail.com>
2018-06-12 22:05:27 -0600

    complete checkpoint A

--- cs165/assignments/check08a.cpp
+++ cs165/assignments/check08a.cpp
@@ -1,9 +1,9 @@
 /***********************************************************************
 * Program:
 *    Checkpoint 08a, Inheritance
-*    Brother {Burton, Falin, Ercanbrack}, CS165
+*    Brother Walker, CS165
 * Author:
-*    your name
+*    Scott Currell
 * Summary: 
 *    Summaries are not necessary for checkpoint assignments.
 * ***********************************************************************/
@@ -16,13 +16,80 @@ using namespace std;
 // in this file.
 
 // TODO: Define your Book class here
+class Book
+{
+   private:
+      string title;
+      string author;
+      int publicationYear;
+   
+   public:
+      void promptBookInfo()
+      {
+         cout << "Title: ";
+         getline(cin, title);
 
+         cout << "Author: ";
+         getline(cin, author);
+
+         cout << "Publication Year: ";
+         cin >> publicationYear;
+         cin.ignore(256, '\n');
+      }
+
+      void displayBookInfo() const
+      {
+         cout << title << " (" << publicationYear << ") by " << author << endl;
+      }
+};
 
 // TODO: Define your TextBook class here
+class TextBook : public Book
+{
+   private:
+      string subject;
 
+   public:
+      void promptSubject()
+      {
+         // called from the Book class
+         promptBookInfo();
+
+         cout << "Subject: ";
+         getline(cin, subject);
+      }
+
+      void displaySubject() const
+      {
+         displayBookInfo();
+
+         cout << "Subject: " << subject << endl;
+      }
+};
 
 // TODO: Add your PictureBook class here
+class PictureBook : public Book
+{
+   private:
+      string illustrator;
 
+   public:
+      void promptIllustrator()
+      {
+         // called from the Book class
+         promptBookInfo();
+
+         cout << "Illustrator: ";
+         getline(cin, illustrator);
+      }
+
+      void displayIllustrator() const
+      {
+         displayBookInfo();
+
+         cout << "Illustrated by " << illustrator << endl;
+      }
+};
 
 
 /**********************************************************************
@@ -32,14 +99,28 @@ using namespace std;
 int main()
 {
    // Declare a Book object here and call its methods
+   Book book;
+
+   book.promptBookInfo();
+   cout << endl;
+   book.displayBookInfo();
+   cout << endl;
 
 
    // Declare a TextBook object here and call its methods
+   TextBook textBook;
 
+   textBook.promptSubject();
+   cout << endl;
+   textBook.displaySubject();
+   cout << endl;
 
    // Declare a PictureBook object here and call its methods
+   PictureBook pictureBook;
 
-
+   pictureBook.promptIllustrator();
+   cout << endl;
+   pictureBook.displayIllustrator();
 
    return 0;
 }
