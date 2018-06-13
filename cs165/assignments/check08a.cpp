5db59b19e7e36c5b558a3d7a7fa0e8cdf95e39d7
Scott Currell <scottcurrell@gmail.com>
2018-06-12 20:24:13 -0600

    check 08 A skeleton file

---
+++ cs165/assignments/check08a.cpp
@@ -0,0 +1,47 @@
+/***********************************************************************
+* Program:
+*    Checkpoint 08a, Inheritance
+*    Brother {Burton, Falin, Ercanbrack}, CS165
+* Author:
+*    your name
+* Summary: 
+*    Summaries are not necessary for checkpoint assignments.
+* ***********************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// For this assignment, for simplicity, you may put all of your classes
+// in this file.
+
+// TODO: Define your Book class here
+
+
+// TODO: Define your TextBook class here
+
+
+// TODO: Add your PictureBook class here
+
+
+
+/**********************************************************************
+ * Function: main
+ * Purpose: This is the entry point and driver for the program.
+ ***********************************************************************/
+int main()
+{
+   // Declare a Book object here and call its methods
+
+
+   // Declare a TextBook object here and call its methods
+
+
+   // Declare a PictureBook object here and call its methods
+
+
+
+   return 0;
+}
+
+
