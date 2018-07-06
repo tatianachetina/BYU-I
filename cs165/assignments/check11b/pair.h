5d81e6d0c1f298435fdff876d5af1c22e844dd7d
scott <scottcurrell@gmail.com>
2018-07-05 19:40:27 -0600

    working checkpoint b

--- cs165/assignments/check11b/pair.h
+++ cs165/assignments/check11b/pair.h
@@ -1,5 +1,6 @@
 /*****************************************************
  * File: pair.h
+ * Author: Scott Currell
  *
  * Purpose: Defines a Pair template class that can hold
  *   two items of arbitrary data types.
@@ -10,6 +11,23 @@
 
 // TODO: Fill in this class
 // Put all of your method bodies right in this file
-
+template <class T1, class T2>
+class Pair
+{
+   public:
+      // getters
+      T1 getFirst()  const { return t1; }
+      T2 getSecond() const { return t2; }
+      // setters
+      void setFirst(const T1 & t1)  { this -> t1 = t1; }
+      void setSecond(const T2 & t2) { this -> t2 = t2; }
+      // print to screen
+      void display() {
+         cout << t1 << " - " << t2;
+      };
+   private:
+      T1 t1;
+      T2 t2;
+};
 
 #endif // PAIR_H
